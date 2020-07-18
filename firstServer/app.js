e7d5e5e1a48fa510f5b47e2a3b3cd4da3d9234c4
prateek-parashar <parashar4111@gmail.com>
2020-07-19 00:47:00 +0530

    made the file write asynchronous

--- firstServer/app.js
+++ firstServer/app.js
@@ -26,15 +26,25 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
         });
 
-        req.on("close", () => {
+        // return req.on("end", () => {
+        //     const parsedBody = Buffer.concat(body).toString();
+        //     const message = parsedBody.split("=")[1];
+        //     fs.writeFile("message.txt", message, (err) => {
+        //         res.statusCode = 302;
+        //         res.setHeader("Location", "/");
+        //         return res.end();
+        //     });
+        // });
+
+        return req.on("end", () => {
             let inputMessageBuffer = Buffer.concat(body).toString();
             let inputMessage = inputMessageBuffer.split("=")[1];
-            fs.writeFileSync("message.txt", inputMessage);
+            fs.writeFile("message.txt", inputMessage, (err) => {
+                // The below method can also be written in 2 lines by setting the status and headers indivisually
+                res.writeHead(302, { Location: "/" });
+                return res.end();
+            });
         });
-
-        // The below method can also be written in 2 lines by setting the status and headers indivisually
-        res.writeHead(302, { Location: "/" });
-        return res.end();
     }
 
     res.setHeader("Content-Type", "text/html");
@@ -58,4 +68,13 @@ server.listen(3000);
 
 /**
  * The response is so much like the printerWriter that's used in Java
+ */
+
+/**
+ * Node is Asynchronous in nature and it promotes the event driven architecture where we
+ * give it callback functions which it dumps to the CPU, carries on with the execution of the rest
+ * of the code, and then when the main stack is empty, it executes all the callback functions.
+ *
+ * Here, the code was changed from fs.writeFileSync() to fs.writeFile() because the Synchronous code is
+ * generally avoided to prevent blocking of the thread.
  */
