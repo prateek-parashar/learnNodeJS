46fddb9094aa77a42348195783342ab115a3a624
prateek-parashar <parashar4111@gmail.com>
2020-07-22 01:46:26 +0530

    Adding html pages

--- expressApp/routes/shop.js
+++ expressApp/routes/shop.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
+const path = require("path");
+
 const router = express.Router();
 
-router.use("/welcome", (req, res, next) => {
-    res.send("<h1> Hello From ExpressJS </h1>");
+router.use("/", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
-
-
 
 module.exports = router;
