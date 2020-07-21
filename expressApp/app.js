1f2ea26e2fb00fab2b0d7c6997b8cd769c819444
prateek-parashar <parashar4111@gmail.com>
2020-07-22 01:01:25 +0530

    Changed styling

--- expressApp/app.js
+++ expressApp/app.js
@@ -11,7 +11,9 @@ app.use(shopRoutes);
 
 // Handling the not found requests!
 app.use((req, res, next) => {
-    res.status(404).send("<img src='https://http.cat/404'/>");
+    res.status(404).send(
+        "<div style='text-align:center'> <img src='https://http.cat/404'/> </div>"
+    );
 });
 
 app.listen(3000);
