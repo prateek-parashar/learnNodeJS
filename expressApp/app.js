3f30c6e43774dbde2de9de002830add658929fc6
prateek-parashar <parashar4111@gmail.com>
2020-07-22 22:25:35 +0530

    Added the 404 html page

--- expressApp/app.js
+++ expressApp/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -16,9 +18,7 @@ app.get("/", (req, res) => {
 
 // Handling the not found requests!
 app.use((req, res, next) => {
-    res.status(404).send(
-        "<div style='text-align:center'> <img src='https://http.cat/404'/> </div>"
-    );
+    res.sendFile(path.join(__dirname, "views", "error404.html"));
 });
 
 app.listen(3000);
