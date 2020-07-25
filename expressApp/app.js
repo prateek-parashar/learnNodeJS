2c749f2c665c6cf588a38d04de6e4c314042ab12
prateek-parashar <parashar4111@gmail.com>
2020-07-25 14:30:24 +0530

    Added the image

--- expressApp/app.js
+++ expressApp/app.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 // Handling the not found requests!
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, "views", "error404.html"));
+    res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
