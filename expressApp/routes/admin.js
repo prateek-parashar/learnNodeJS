6325b19f553e6c3b6fb9ddd0428c882c36862f46
prateek-parashar <parashar4111@gmail.com>
2020-08-11 00:34:36 +0530

    Added route protection

--- expressApp/routes/admin.js
+++ expressApp/routes/admin.js
@@ -1,22 +1,23 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin");
+const authCheck = require("../middleware/authCheck");
 
 const router = express.Router();
 
 // Creating the routes. Notice the methods used - app.get() and app.post()
 // Appropriate controllers are called to handle the request
-router.get("/add-product", adminController.getAddProduct);
+router.get("/add-product", authCheck, adminController.getAddProduct);
 
-router.post("/add-product", adminController.postProduct);
+router.post("/add-product", authCheck, adminController.postProduct);
 
-router.get("/products", adminController.getProducts);
+router.get("/products", authCheck, adminController.getProducts);
 
-router.get("/edit-product/:productID", adminController.getEditProduct);
+router.get("/edit-product/:productID", authCheck, adminController.getEditProduct);
 
-router.post("/edit-product", adminController.editProduct);
+router.post("/edit-product", authCheck, adminController.editProduct);
 
-router.post("/delete-product", adminController.deleteProduct);
+router.post("/delete-product", authCheck, adminController.deleteProduct);
 
 // Here, we export an object whose keys will be the keys we set here for the exports object.
 module.exports = router;
--- expressApp/routes/shop.js
+++ expressApp/routes/shop.js
@@ -3,6 +3,7 @@ const express = require("express");
 // Creating the routes. Notice the methods used - app.get() and app.post()
 // Appropriate controllers are called to handle the request
 const shopController = require("../controllers/shop");
+const authCheck = require("../middleware/authCheck");
 
 const adminData = require("./admin");
 
@@ -16,17 +17,17 @@ router.get("/products", shopController.getProductList);
 
 router.get("/product/:productID", shopController.getProduct);
 
-router.get("/cart", shopController.getCart);
+router.get("/cart", authCheck, shopController.getCart);
 
-router.post("/cart", shopController.addToCart);
+router.post("/cart", authCheck, shopController.addToCart);
 
-router.post("/cart-delete-item", shopController.deleteFromCart);
+router.post("/cart-delete-item", authCheck, shopController.deleteFromCart);
 
-router.get("/orders", shopController.getOrders);
+router.get("/orders", authCheck, shopController.getOrders);
 
-router.post("/create-order", shopController.addOrder);
+router.post("/create-order", authCheck, shopController.addOrder);
 
-router.get("/checkout", shopController.getCheckout);
+router.get("/checkout", authCheck, shopController.getCheckout);
 
 // Here, we are basically exporting an object whose keys will be the keys we set here for the exports object.
 module.exports = router;
