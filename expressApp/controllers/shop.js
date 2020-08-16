9bd0355748cf9aaf315cc40aa125bd4e8fd13498
prateek-parashar <parashar4111@gmail.com>
2020-08-16 23:36:14 +0530

    Passing the files in a steaming fashion instead of reading them in memory and then serving them

--- expressApp/controllers/shop.js
+++ expressApp/controllers/shop.js
@@ -148,12 +148,17 @@ exports.getInvoice = (req, res, next) => {
 
     const invoicePath = path.join("data", "invoices", invoiceName);
 
-    fs.readFile(invoicePath, (err, data) => {
-        if (err) {
-            return next(err);
-        }
-        res.setHeader("Content-Type", "application/jpg");
-        res.setHeader("Content-Disposition", "inline; filename=" + invoiceName + "");
-        res.send(data);
-    });
+    // fs.readFile(invoicePath, (err, data) => {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.setHeader("Content-Type", "application/jpg");
+    //     res.setHeader("Content-Disposition", "inline; filename=" + invoiceName + "");
+    //     res.send(data);
+    // });
+
+    const file = fs.createReadStream(invoicePath);
+    res.setHeader("Content-Type", "application/jpg");
+    res.setHeader("Content-Disposition", "inline; filename=" + invoiceName + "");
+    file.pipe(res);
 };
